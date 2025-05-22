0d488cb1d2a438ca3a69f0c7e40c444aedcdc688
prakash <prakash@digivalsolutions.com>
2025-05-22 11:27:42 +0530

    add skeleton

--- src/Utils/ReactUtile.jsx
+++ src/Utils/ReactUtile.jsx
@@ -1,5 +1,12 @@
 import { Suspense } from "react";
 
 export const SuspenseCall = (props) => (
-    <Suspense fallback={"loading"}>{props.children}</Suspense>
+    <Suspense fallback={<Skeleton/>}>{props.children}</Suspense>
 );
+
+const Skeleton =()=>{
+    return <div className="skeleton">
+        <div className="left-pan"></div>
+        <div className="right-pan"></div>
+    </div>
+}
